2378754837efb308a159ba78e8ca84d90db98e68
Alekael <118826117+Alekael@users.noreply.github.com>
2023-03-24 09:16:26 +0100

    Terminado ejercicio 6
    Terminado hasta el ejercicio 6 con funcionalidad completa

--- app/src/main/java/ui/MainActivity.kt
+++ app/src/main/java/ui/MainActivity.kt
@@ -2,19 +2,50 @@ package ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.core.view.MenuProvider
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 import dadm.alsadel.frasesfamosas.R
 import dadm.alsadel.frasesfamosas.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MenuProvider {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
        // setContentView(R.layout.activity_main)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val navController = binding.fragmentContainerView.getFragment<NavHostFragment>().navController
-        (binding.bottomMenu as BottomNavigationView).setupWithNavController(navController)
+        val navController = binding.fragmentContainerView!!.getFragment<NavHostFragment>().navController
+        //cambiado por navigationbarview
+        (binding.bottomMenu as NavigationBarView).setupWithNavController(navController)
+        //nuevo
+        setSupportActionBar(binding.toolbar)
+        //tras dos imports
+        val barconf = AppBarConfiguration.Builder(navController.graph).build()
+        setupActionBarWithNavController(navController, barconf)
+        // hasta aquí funciona
+        addMenuProvider(this)
+    }
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+
+        menuInflater.inflate(R.menu.menu_about, menu)
+
+    }
+
+
+
+    override fun onMenuItemSelected(menuItem: MenuItem):Boolean{
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val navController = binding.fragmentContainerView!!.getFragment<NavHostFragment>().navController
+        if (menuItem.itemId==R.id.aboutFragment) {
+            navController.navigate(R.id.aboutDialogFragment)
+            return true
+        }
+        return false
     }
 }
